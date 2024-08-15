398d96a08eaa49dbfc5c0aab881536d1bd430f44
Anastasiia <138507200+serputkona@users.noreply.github.com>
2024-08-15 15:55:31 -0400

    Update Time_series_analysis.R

--- Time_series_analysis.R
+++ Time_series_analysis.R
@@ -1,66 +1,81 @@
 getwd()
-Suicidal_Behaviours  <- read.csv("GHSH_Pooled_Data1.csv")
+gs  <- read.csv("GoldSilver.csv")
+gs
+library(astsa)
+library(ggplot2)
+library(TTR)
+library(forecast)
+library(rlang)
+#remove.packages("rlang")
+#remove.packages("ggplot2")
 
-#1
-seredn <- mean(Suicidal_Behaviours$Have_Understanding_Parents)
-seredn
-median(Suicidal_Behaviours$Have_Understanding_Parents)
+gs = read.csv('GoldSilver.csv')
+data = ts(gs$gold, gs$time, start = c(1990, 12), end = c(2010, 12), frequency = 12)
+plot(data)
+
+ggseasonplot(data)
+ggseasonplot(data, polar = T)
+
+ggsubseriesplot(data) +  ggtitle("Діаграма сезонних підрядів")
+
+plot.ts(data)
+plot.ts(SMA(data, n=3))
+plot.ts(SMA(data, n=40))
+plot.ts(SMA(data, n=100))
+
+#lam <- BoxCox.lambda(data)
+#lam
+#Air.2transf <- cbind("Степенева"= BoxCox(data, lam),
+ #"Степінь = 0.99" = BoxCox(data, lambda = .99),
+ #"Логарифмічна" = BoxCox(data, lambda = 0))
+#autoplot(Air.2transf, facets = T)+ xlab("Рік")
+
+DecDATA <- decompose(data)
+plot(DecDATA)
+
+#Корелограма та частинна корелограма для часового ряду
+acf(data)
+pacf(data)
+
+acf(diff(data))
+pacf(diff(data))
+
+#Наївні прогнози: простий, зсунутий, сезонний наївний, на базі середнього значення
+#data.1 <- meanf(data, h=10)
+data.2 <- rwf(data, h=10)
+#data.3 <- snaive(data, h=10)
+data.4 <- rwf(data, drift = TRUE, h=10)
+autoplot(window(data, start=1990)) +
+ #autolayer(data.1, series="Mean", PI=FALSE) +
+ autolayer(data.2, series="Naïve", PI=FALSE) +
+ #autolayer(data.3, series="Seasonal naïve", PI=FALSE) +
+ autolayer(data.4, series="Drift method", PI=FALSE) +
+ xlab("Years") + ylab("CPI") +
+ ggtitle(" ")
 
 
-#2
-var(Suicidal_Behaviours$Have_Understanding_Parents) #дисперсія
-stand_vidh <- sd(Suicidal_Behaviours$Have_Understanding_Parents) #стандартне відхилення
-stand_vidh
-cv <- stand_vidh / seredn * 100 #коефіцієнт варіації
-cv
-min_ <- min(Suicidal_Behaviours$Have_Understanding_Parents)
-max_ <- max(Suicidal_Behaviours$Have_Understanding_Parents)
-max_ - min_
-IQR(Suicidal_Behaviours$Have_Understanding_Parents) #інтерквальний розмах
+#Подвійне експоненційне згладжування(прогноз):
+hw1 <- HoltWinters(data, beta = FALSE, gamma =0, seasonal = c("additive"))
+hw1
+plot(hw1)
+
+data.hw.for <- forecast(hw1, h=12)
+plot(data.hw.for)
+
+?HoltWinters
 
 
-#3
-boxplot(Suicidal_Behaviours$Have_Understanding_Parents,
-        main="Box-and-whiskers plot:Adolescents with Suicidal Behaviours
-        and Understanding with their parents   ",ylab="Percents(%)", col="coral2")
+d= diff(data)
+arima.d <- auto.arima(d)
+autoplot(forecast(arima.d))
 
-#4
-quantile(Suicidal_Behaviours$Have_Understanding_Parents)
+res.data.naive <- residuals(snaive(diff(data)))
+autoplot(res.data.naive)
 
-#5
-quantile(Suicidal_Behaviours$Have_Understanding_Parents, prob = c(0.1, 0.9)) 
+gghistogram(res.data.naive) + ggtitle("Гістограма залишків")
 
-#6
-library(psych)
-describe(Suicidal_Behaviours$Have_Understanding_Parents)[11] #коефіцієнт асиметрії
-describe(Suicidal_Behaviours$Have_Understanding_Parents)[12] #коефіцієнт ексцесу
+ggAcf(res.data.naive)+ ggtitle("Корелограма залишків")
 
-#7
-hist(Suicidal_Behaviours$Have_Understanding_Parents,
-     freq=F, main="Sturges rule", col="darkorchid1")
-hist(Suicidal_Behaviours$Have_Understanding_Parents, breaks = "Scott",
-     freq=F, main="Scott rule", col = "lightpink1", 
-     xlab = "Understanding with parents", ylab = "relative frequency")
-hist(Suicidal_Behaviours$Have_Understanding_Parents, breaks = "FD", freq=F, main="FD rule",
-     col = "darkseagreen3", xlab = "Understanding with parents", ylab = "relative frequency")
-lines(density(Suicidal_Behaviours$Have_Understanding_Parents), col='chocolate2', lwd=5)
-plot(density(Suicidal_Behaviours$Have_Understanding_Parents), col='cornflowerblue', lwd=5) #графік щільності
+Box.test(res.data.naive, lag=20)
 
-#8
-library(car)
-qqnorm(Suicidal_Behaviours$Have_Understanding_Parents, pch = 3, frame = FALSE, col="lightblue4")
-qqline(Suicidal_Behaviours$Have_Understanding_Parents, col = "lightcoral", lwd = 3)
-
-#9
-plot(pnorm(sort(x)), (1:length(x))/length(x),
-     xlab="Теоретична функція розподілу", ylab = "Емпірична функція
-розподілу", asp = 1)
-abline(0, 1, col="red")
-
-plot(pnorm(sort((Suicidal_Behaviours$Have_Understanding_Parents  - mean(Suicidal_Behaviours$Have_Understanding_Parents)) / sd(Suicidal_Behaviours$Have_Understanding_Parents))),
-     (1:length(Suicidal_Behaviours$Have_Understanding_Parents)) / length(Suicidal_Behaviours$Have_Understanding_Parents),xlab="Теоретична функція розподілу",ylab="Імпірична функція розподілу",asp=1)
-abline(0, 1, col="green2") 
-
-#10
-shapiro.test(Suicidal_Behaviours$Have_Understanding_Parents)  
-#критерій згоди Колмогорова-Смірнова для неперервних розподілів та критерій нормальності Шапіро-Уілка.
+checkresiduals(snaive(diff(data)))
